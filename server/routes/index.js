7cbfd90c6d2e1794fcbce7f80e6aab1197284767
dimalei <dima.leigr@gmail.com>
2025-04-09 20:19:41 +0200

    hello world post request

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', (req, res) => {
+  res.send('Got a Post Request!')
+})
+
 
 module.exports = router;
