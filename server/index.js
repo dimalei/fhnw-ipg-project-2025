881e06f9722b6703cac83835daf83d92777db2af
dimalei <dima.leigr@gmail.com>
2025-04-22 22:03:46 +0200

    chat POC

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+const { join } = require("node:path");
+
 const app = express();
 const port = 8080;
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" },
 });
 
-app.listen(port, () => {
-  console.log("Server listening on port ${port}");
+io.on("connection", (socket) => {
+  console.log(`a user connected ${socket.id}`);
+
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${socket.id} said ${message}`);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port http://localhost:${port}`);
 });
