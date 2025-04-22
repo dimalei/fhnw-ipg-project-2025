9577dcb7c97c93a1e0bc4ce97a92235e22c12861
dimalei <dima.leigr@gmail.com>
2025-04-22 21:04:49 +0200

    fresh start

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port ${port}");
+});
